d2b60eb56d2e93b691eeb52690fda146b4853058
Crowdbotics <team@crowdbotics.com>
2023-06-14 04:39:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-queen-41460",
+  url: "https://falling-queen-41460.botics.co",
+  api: "https://falling-queen-41460.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
